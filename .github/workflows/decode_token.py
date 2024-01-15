c9da18a86b580664822e40bb3ab856d8b9fea375
btsil <billt@silicom-usa.com>
2024-01-15 12:05:44 -0500

    update 69

---
+++ .github/workflows/decode_token.py
@@ -0,0 +1,37 @@
+import json
+import base64
+import sys
+import time
+import jwt
+import requests
+import os
+
+def decode_token(token):
+    decoded_text = base64.b64decode(token).decode('utf-8')
+    data = json.loads(decoded_text)
+    APP_ID = data["str1"]
+    INSTALLATION_ID = data["str2"]
+    PEM = data["text"]
+
+    now = int(time.time())
+    iat = now - 60
+    exp = now + 60
+    header = {"alg": "RS256", "typ": "JWT"}
+    payload = {"iat": iat, "exp": exp, "iss": APP_ID}
+    jwt_token = jwt.encode(payload, PEM, algorithm="RS256", headers=header)
+
+    url = f"https://api.github.com/app/installations/{INSTALLATION_ID}/access_tokens"
+    headers = {
+        "Authorization": f"Bearer {jwt_token}",
+        "Accept": "application/vnd.github+json",
+        "X-GitHub-Api-Version": "2022-11-28"
+    }
+
+    response = requests.post(url, headers=headers)
+    INSTALLATION_ACCESS_TOKEN = response.json()['token']
+    return INSTALLATION_ACCESS_TOKEN
+
+if __name__ == "__main__":
+    token = sys.argv[1]
+    access_token = decode_token(token)
+    print(access_token)
